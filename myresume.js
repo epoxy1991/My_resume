edd3fc1e9805db6ea3071260c8ac6bf2be9cc460
victor victor <victor.mogo@yahoo.com>
2021-06-19 22:32:17 -0700

    added files

---
+++ myresume.js
@@ -0,0 +1,15 @@
+$('body').scrollspy({target: ".navbar", offset: 50});
+
+$("#mySidenav a").on('click', function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+         window.location.hash = hash;   
+        });
+    }
+});
