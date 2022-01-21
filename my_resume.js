178a3851f48e910d06a529511b3492acf67ffc2f
Victor Mogo <Victor.mogo@yahoo.com>
2022-01-21 11:38:41 -0500

    new commit

--- my_resume.js
+++ my_resume.js
@@ -2,9 +2,22 @@ $(document).ready(function(){
    $("#btnhide").click(function(){
       $("#mySidenav").hide()
       $("#main").css('margin-left',0);
+   
    });
    $("#btnshow").click(function(){
     $("#mySidenav").show()
    //  $("#main").css('margin-left',250);
  });
 });
+
+const navSlide =() => {
+   const burger = document.querySelector('.burger');
+   const nav = document.querySelector('.nav-links');
+
+   burger.addEventListener('click', ()=>{
+       nav.classList.toggle('nav-active');
+       burger.classList.toggle('bread');
+   });
+}
+
+navSlide();
